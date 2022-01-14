86b12fed12c6f74b89d7e698a2213ca4a6469315
sakib00 <sakib.xc@gmail.com>
2022-01-15 00:03:39 +0600

    Api working and minor card changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 import { ListItemComponent } from './components/ListItemComponent';
 
 function App() {
+  const [authors, setAuthors] = useState([]);
+
+  const getAuthorRequest = async () => {
+    const url = `//api.quotable.io/authors`;
+
+    const response = await fetch(url);
+    const responseJson = await response.json();
+
+    console.log(response);
+    console.log(responseJson);
+
+    if (responseJson.results) {
+      setAuthors(responseJson.results);
+    } else {
+      setAuthors([]);
+    }
+    console.log(authors);
+  };
+
+  useEffect(() => {
+    getAuthorRequest();
+  }, []);
+
   return (
-    <div className='App bg-primary min-h-screen'>
-      <ListItemComponent />
+    <div className='App min-h-screen'>
+      <div className='body container mx-auto'>
+        <div className='flex'>
+          <div className='list grid grid-cols-4 gap-6'>
+            {authors.length !== 0 ? (
+              authors.map((author) => <ListItemComponent author={author} />)
+            ) : (
+              <div className='flex items-center'>
+                <h1 className='text-sm font-semibold  my-5'>
+                  No Authors found.
+                </h1>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/ListItemComponent.js
+++ src/components/ListItemComponent.js
@@ -1,28 +1,17 @@
 import React from 'react';
 
-export const ListItemComponent = () => {
+export const ListItemComponent = ({ author }) => {
   return (
-    <div className='container mx-auto'>
-      <div className='border rounded-md flex px-8 py-4 space-x-3 border-gray-600'>
-        <div className='my-4 space-y-4'>
-          <h3 className='text-lg text-light font-bold mb-2'>John Doe</h3>
-          <a
-            href='www.google.com'
-            className='text-sm tracking-wide text-blue-500 hover:text-blue-800 visited:text-purple-600 '
-          >
-            www.wiki.com
-          </a>
-          <p className='text-sm text-gray-300'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-            consequuntur aut nam labore, necessitatibus temporibus mollitia
-            provident quis officia dolorem aliquam tenetur sequi ducimus
-            molestias reprehenderit nulla quod, delectus architecto.
-          </p>
-        </div>
-        <div className='my-4'>
-          <div className='border rounded-full p-2 border-gray-500 text-secondary-dark hover:border-gray-300 hover:text-secondary cursor-pointer'>
-            {/* Outline */}
-            {/* <svg
+    <div className='px-8 py-4 border rounded-lg  bg-white border-gray-100 shadow-lg'>
+      <div className='my-4 space-y-4'>
+        <div className='flex items-center justify-between pb-2 border-b border-gray-200 space-x-3'>
+          <div>
+            <h3 className='text-lg font-bold'>{author.name}</h3>
+          </div>
+          <div className=''>
+            <div className='border rounded-full p-2 border-gray-300 text-secondary-dark  hover:bg-secondary-dark hover:text-white cursor-pointer'>
+              {/* Outline */}
+              {/* <svg
             xmlns='http://www.w3.org/2000/svg'
             className='h-4 w-4 '
             fill='none'
@@ -36,21 +25,31 @@ export const ListItemComponent = () => {
               d='M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z'
             />
           </svg> */}
-            {/* Solid */}
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              className='h-4 w-4 '
-              viewBox='0 0 20 20'
-              fill='currentColor'
-            >
-              <path
-                fillRule='evenodd'
-                d='M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z'
-                clipRule='evenodd'
-              />
-            </svg>
+              {/* Solid */}
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='h-4 w-4 '
+                viewBox='0 0 20 20'
+                fill='currentColor'
+              >
+                <path
+                  fillRule='evenodd'
+                  d='M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z'
+                  clipRule='evenodd'
+                />
+              </svg>
+            </div>
           </div>
         </div>
+        <div className=''>
+          <a
+            href='www.google.com'
+            className='text-sm tracking-wide text-blue-500 hover:text-blue-800 visited:text-purple-600'
+          >
+            {author.link}
+          </a>
+        </div>
+        <p className='text-sm '>{author.bio}</p>
       </div>
     </div>
   );
